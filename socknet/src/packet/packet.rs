7023cbb1980aaf13c9f10eef5cd9645ce6ba5250
temportalflux <dustin.yost.t@gmail.com>
2021-09-19 15:26:50 -0400

    Add support for user-defined packet sending mode in the packet builder

--- socknet/src/packet/packet.rs
+++ socknet/src/packet/packet.rs
@@ -1,9 +1,37 @@
 use super::{DeliveryGuarantee, Guarantee, Kind, OrderGuarantee, Payload};
-use std::net::{IpAddr, Ipv4Addr, SocketAddr, ToSocketAddrs};
+use std::{
+	collections::HashSet,
+	net::{SocketAddr, ToSocketAddrs},
+};
+
+#[derive(Clone)]
+pub enum PacketMode {
+	/// Packet is sent to the specified addresses.
+	Directed,
+	// Packet is sent to whatever the application deems as the "server".
+	ToServer,
+	// Packet is sent to all connections.
+	Broadcast,
+}
+
+impl Default for PacketMode {
+	fn default() -> Self {
+		PacketMode::Directed
+	}
+}
+
+pub trait AddressReference {
+	fn local_address(&self) -> &SocketAddr;
+	fn active_addresses(&self) -> Vec<SocketAddr>;
+	fn server_address(&self) -> SocketAddr;
+}
 
 #[derive(Clone)]
 pub struct PacketBuilder {
-	address: SocketAddr,
+	mode: PacketMode,
+	ignore_local_address: bool,
+	include_addresses: HashSet<SocketAddr>,
+	exclude_addresses: HashSet<SocketAddr>,
 	guarantee: Guarantee,
 	payload: Payload,
 }
@@ -13,7 +41,10 @@ impl Default for PacketBuilder {
 		use DeliveryGuarantee::*;
 		use OrderGuarantee::*;
 		Self {
-			address: SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8000),
+			mode: PacketMode::default(),
+			ignore_local_address: false,
+			include_addresses: HashSet::new(),
+			exclude_addresses: HashSet::new(),
 			guarantee: Unreliable + Unordered,
 			payload: Payload::default(),
 		}
@@ -21,20 +52,55 @@ impl Default for PacketBuilder {
 }
 
 impl PacketBuilder {
-	pub fn with_address<T>(mut self, address: T) -> std::io::Result<Self>
-	where
-		T: ToSocketAddrs,
-	{
-		self.set_address(address)?;
-		Ok(self)
+	pub fn with_mode(mut self, mode: PacketMode) -> Self {
+		self.mode = mode;
+		self
 	}
 
-	pub fn set_address<T>(&mut self, address: T) -> std::io::Result<()>
+	fn item_as_address<T>(address: T) -> std::io::Result<SocketAddr>
 	where
 		T: ToSocketAddrs,
 	{
 		let mut iter = address.to_socket_addrs()?;
-		self.address = iter.next().unwrap();
+		Ok(iter.next().unwrap())
+	}
+
+	pub fn ignore_local_address(mut self) -> Self {
+		self.ignore_local_address = true;
+		self
+	}
+
+	pub fn with_address<T>(mut self, address: T) -> std::io::Result<Self>
+	where
+		T: ToSocketAddrs,
+	{
+		self.include_address(address)?;
+		Ok(self)
+	}
+
+	pub fn include_address<T>(&mut self, address: T) -> std::io::Result<()>
+	where
+		T: ToSocketAddrs,
+	{
+		self.include_addresses
+			.insert(Self::item_as_address(address)?);
+		Ok(())
+	}
+
+	pub fn without_address<T>(mut self, address: T) -> std::io::Result<Self>
+	where
+		T: ToSocketAddrs,
+	{
+		self.exclude_address(address)?;
+		Ok(self)
+	}
+
+	pub fn exclude_address<T>(&mut self, address: T) -> std::io::Result<()>
+	where
+		T: ToSocketAddrs,
+	{
+		self.exclude_addresses
+			.insert(Self::item_as_address(address)?);
 		Ok(())
 	}
 
@@ -55,12 +121,46 @@ impl PacketBuilder {
 		self.payload.kind()
 	}
 
-	pub fn build(self) -> Packet {
-		Packet {
-			address: self.address,
-			guarantee: self.guarantee,
-			payload: self.payload,
+	pub fn into_addresses<T: AddressReference>(&self, reference: &T) -> Vec<SocketAddr> {
+		let local_address = reference.local_address();
+		match self.mode {
+			PacketMode::Directed => self
+				.include_addresses
+				.iter()
+				.filter_map(|address| {
+					if self.ignore_local_address && *address == *local_address {
+						None
+					} else {
+						Some(address.clone())
+					}
+				})
+				.collect(),
+			PacketMode::ToServer => vec![reference.server_address()],
+			PacketMode::Broadcast => reference
+				.active_addresses()
+				.into_iter()
+				.filter_map(|active_address| {
+					if self.ignore_local_address && active_address == *local_address {
+						return None;
+					}
+					if self.exclude_addresses.contains(&active_address) {
+						return None;
+					}
+					Some(active_address)
+				})
+				.collect(),
 		}
+	}
+
+	pub fn into_packets<T: AddressReference>(self, reference: &T) -> Vec<Packet> {
+		self.into_addresses(reference)
+			.into_iter()
+			.map(|address| Packet {
+				address,
+				guarantee: self.guarantee.clone(),
+				payload: self.payload.clone(),
+			})
+			.collect()
 	}
 }
 
@@ -102,18 +202,6 @@ impl From<laminar::Packet> for Packet {
 impl Packet {
 	pub fn builder() -> PacketBuilder {
 		PacketBuilder::default()
-	}
-
-	pub fn into_builder(self) -> PacketBuilder {
-		PacketBuilder {
-			address: self.address,
-			guarantee: self.guarantee,
-			payload: self.payload,
-		}
-	}
-
-	pub fn as_builder(&self) -> PacketBuilder {
-		self.clone().into_builder()
 	}
 
 	pub fn address(&self) -> &SocketAddr {
