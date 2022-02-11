fbe0a0af103b8f39a94a536b623117497282ee76
temportalflux <dustin.yost.t@gmail.com>
2022-02-11 12:44:02 -0500

    Remove laminar dependency

--- socknet/src/lib.rs
+++ socknet/src/lib.rs
@@ -5,26 +5,10 @@ pub static LOG: &'static str = "socknet";
 #[cfg(feature = "derive")]
 pub use socknet_derive::*;
 
-// OLD
-pub mod event;
-pub mod packet;
-pub mod socket;
-pub mod backend {
-	pub use laminar::{Config, Connection, ConnectionManager, Packet, SocketEvent as Event};
-}
-pub(crate) enum InternalMessage {
-	DropConnection(std::net::SocketAddr),
-}
-pub mod serde {
-	pub use rmp_serde::{from_read_ref, to_vec};
-}
-
-// NEW
 mod config;
 pub use config::*;
 
 pub mod connection;
 pub mod endpoint;
-pub mod message;
 pub mod stream;
 pub mod utility;
